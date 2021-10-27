58d5b47139636b1929fa513cdba7bbafcc094d2f
jiangyu <jiangyu53231323@163.com>
2021-10-27 21:59:32 +0800

    修改scrnet网络结构

--- nets/scrnet.py
+++ nets/scrnet.py
@@ -25,6 +25,37 @@ class hswish(nn.Module):
         out = x * F.relu6(x + 3, inplace=True) / 6
         return out
 
+
+class SeModule(nn.Module):
+    def __init__(self, in_size, reduction=4):
+        super(SeModule, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_size, in_size // reduction, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size // reduction),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_size // reduction, in_size, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size),
+            hsigmoid()
+        )
+
+
+class DP_Conv(nn.Module):
+    def __init__(self, kernel_size, in_size, out_size, nolinear, stride):
+        super(DP_Conv, self).__init__()
+        self.conv1 = nn.Conv2d(in_size, in_size, kernel_size=kernel_size, stride=stride,
+                               padding=kernel_size // 2, groups=in_size, bias=False)
+        self.bn1 = nn.BatchNorm2d(in_size)
+        self.nolinear1 = nolinear
+        self.conv2 = nn.Conv2d(in_size, out_size, kernel_size=1, stride=1, padding=0, bias=False)
+        # self.bn2 = nn.BatchNorm2d(out_size)
+
+    def forward(self, x):
+        out = self.nolinear1(self.bn1(self.conv1(x)))
+        out = self.conv2(out)
+        return out
+
+
 # Residual block 残差块
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
@@ -95,6 +126,44 @@ class Res_block2_Conv_BN_ReLU(nn.Module):
         return out
 
 
+class Block(nn.Module):
+    '''expand + depthwise + pointwise'''
+
+    def __init__(self, kernel_size, in_size, expand_size, out_size, nolinear, semodule, stride):
+        super(Block, self).__init__()
+        self.stride = stride
+        if semodule != None:
+            self.se = semodule(out_size)
+        else:
+            self.se = None
+
+        self.conv1 = nn.Conv2d(in_size, expand_size, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(expand_size)
+        self.nolinear1 = nolinear
+        self.conv2 = nn.Conv2d(expand_size, expand_size, kernel_size=kernel_size, stride=stride,
+                               padding=kernel_size // 2, groups=expand_size, bias=False)
+        self.bn2 = nn.BatchNorm2d(expand_size)
+        self.nolinear2 = nolinear
+        self.conv3 = nn.Conv2d(expand_size, out_size, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_size)
+
+        self.shortcut = nn.Sequential()
+        if stride == 1 and in_size != out_size:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_size, out_size, kernel_size=1, stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(out_size),
+            )
+
+    def forward(self, x):
+        out = self.nolinear1(self.bn1(self.conv1(x)))
+        out = self.nolinear2(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        if self.se != None:
+            out = self.se(out)
+        out = out + self.shortcut(x) if self.stride == 1 else out
+        return out
+
+
 # 主干网络
 class SCRNet(nn.Module):
     def __init__(self):
@@ -109,51 +178,73 @@ class SCRNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(16)
         self.hs1 = hswish()
 
-        self.stage1 = nn.Sequential(
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(16, 93),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 93),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 93),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 93),
+        # self.stage1 = nn.Sequential(
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(16, 93),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 93),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 93),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 93),
+        # )
+        # self.stage2 = nn.Sequential(
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 176, stride=2),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 176),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 176),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 176),
+        # )
+        # self.stage3 = nn.Sequential(
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 256, stride=2),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(256, 256),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(256, 256),
+        #     BN_Conv3_BN_ReLU_Conv3_BN_Dy(256, 256),
+        # )
+        self.bneck1 = nn.Sequential(
+            Block(3, 16, 16, 16, nn.ReLU(inplace=True), SeModule, 2),
         )
-        self.stage2 = nn.Sequential(
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(93, 176, stride=2),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 176),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 176),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 176),
+        self.bneck2 = nn.Sequential(
+            Block(3, 16, 72, 24, nn.ReLU(inplace=True), None, 2),
+            Block(3, 24, 88, 24, nn.ReLU(inplace=True), None, 1),
         )
-        self.stage3 = nn.Sequential(
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(176, 256, stride=2),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(256, 256),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(256, 256),
-            BN_Conv3_BN_ReLU_Conv3_BN_Dy(256, 256),
+        self.bneck3 = nn.Sequential(
+            Block(5, 24, 96, 40, hswish(), SeModule, 2),
+            Block(5, 40, 240, 40, hswish(), SeModule, 1),
+            Block(5, 40, 240, 40, hswish(), SeModule, 1),
+            Block(5, 40, 120, 48, hswish(), SeModule, 1),
+            Block(5, 48, 144, 48, hswish(), SeModule, 1),
         )
-        self.conv2 = nn.Sequential(
-            conv3x3(256, 256),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=(8, 1),
-                      stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
+        self.bneck4 = nn.Sequential(
+            Block(5, 48, 288, 96, hswish(), SeModule, 2),
+            Block(5, 96, 576, 96, hswish(), SeModule, 1),
+            Block(5, 96, 576, 96, hswish(), SeModule, 1),
         )
-        self.stage4 = nn.Sequential(
-            Res_block2_Conv_BN_ReLU(256, 256),
-            Res_block2_Conv_BN_ReLU(256, 256),
-        )
+        # self.conv2 = nn.Sequential(
+        #     conv3x3(256, 256),
+        #     nn.BatchNorm2d(256),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(256, 256, kernel_size=(8, 1),
+        #               stride=1, padding=0, bias=False),
+        #     nn.BatchNorm2d(256),
+        #     nn.ReLU(inplace=True),
+        # )
+        # self.stage4 = nn.Sequential(
+        #     Res_block2_Conv_BN_ReLU(256, 256),
+        #     Res_block2_Conv_BN_ReLU(256, 256),
+        # )
+        self.conv2 = DP_Conv(3, 96, 128, hswish(), 1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.hs2 = hswish()
         self.classifier1 = nn.Sequential(
-            nn.Conv2d(256, 34, kernel_size=(1, 8),
+            nn.Conv2d(128, 34, kernel_size=(1, 8),
                       stride=(1, 4), padding=0, bias=False),
             nn.BatchNorm2d(34),
             nn.ReLU(inplace=True),
         )
         self.classifier2 = nn.Sequential(
-            nn.Conv2d(256, 25, kernel_size=(1, 8),
+            nn.Conv2d(128, 25, kernel_size=(1, 8),
                       stride=(1, 4), padding=0, bias=False),
             nn.BatchNorm2d(25),
             nn.ReLU(inplace=True),
         )
         self.classifier3 = nn.Sequential(
-            nn.Conv2d(256, 35, kernel_size=(1, 8),
+            nn.Conv2d(128, 35, kernel_size=(1, 8),
                       stride=(1, 4), padding=0, bias=False),
             nn.BatchNorm2d(35),
             nn.ReLU(inplace=True),
